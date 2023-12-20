bb0502985b979cd996184d8915c01d0f2957b26d
SofiyaYehorova <yehorova66@gmail.com>
2023-12-20 13:01:06 +0200

    main

---
+++ src/main/java/sofiya/javaspringmongodb/JavaSpringMongodbApplication.java
@@ -0,0 +1,13 @@
+package sofiya.javaspringmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringMongodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringMongodbApplication.class, args);
+    }
+
+}
